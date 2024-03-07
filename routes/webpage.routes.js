7d91cb63496fef1522e774e6d6e88626a897eb52
RizwanAmjad <rizwanamjadms@gmail.com>
2024-03-07 21:04:28 +0500

    Updated the repo to add an update endpoint for updating the webpage content. This reflects the changes in the pinata ipfs as well as the DB

--- routes/webpage.routes.js
+++ routes/webpage.routes.js
@@ -51,6 +51,34 @@ router.get("/", auth, async (req, res) => {
   return res.send({ webpage: webpageJson })
 })
 
+router.put("/", auth, async (req, res) => {
+  const { user, body } = req
+
+  const error = validateCreateWebpage(body)
+  if (error) return res.status(400).send(error)
+
+  const dbWebpage = await Webpage.findOne({ user: user._id })
+  if (!dbWebpage) return res.status(404).send("Webpage not found")
+
+  const { cid } = dbWebpage
+  await deleteJsonFromFileStorage(cid)
+
+  const jsonData = JSON.stringify(body)
+  const fileBlob = new Blob([jsonData], { type: "application/json" })
+  const file = new File([fileBlob], `${uuidv4()}.json`, {
+    type: "application/json",
+  })
+
+  const newCid = await uploadJsonToFileStorage(file)
+
+  const webpage = await Webpage.findByIdAndUpdate(dbWebpage._id, {
+    cid: newCid,
+  })
+  await webpage.save()
+
+  return res.send({ webpage })
+})
+
 router.delete("/", auth, async (req, res) => {
   const { user } = req
 
