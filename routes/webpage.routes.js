adcc52473d6336570a9400d97f98dd86c9de9179
RizwanAmjad <rizwanamjadms@gmail.com>
2024-03-20 15:55:13 +0500

    Finished the EJS template stuff

--- routes/webpage.routes.js
+++ routes/webpage.routes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs")
+const ejs = require("ejs")
 const { v4: uuidv4 } = require("uuid");
 
 const auth = require("../middlewares/auth.middleware");
@@ -41,16 +43,179 @@ router.post("/", auth, async (req, res) => {
   return res.send({ webpage });
 });
 
-router.get("/", auth, async (req, res) => {
-  const { user } = req;
+router.get("/", async (req, res) => {
+  const templateFile = fs.readFileSync(`${__dirname}/../template/index.ejs`)
 
-  const webpage = await Webpage.findOne({ user: user._id });
-  if (!webpage) return res.status(404).send("Webpage not found");
-
-  const { cid } = webpage;
-  const webpageJson = await getFromFileStorage(cid);
-
-  return res.send({ webpage: webpageJson });
+  const template = ejs.compile(templateFile.toString())
+  const renderedTemplate = template({
+    "navbar": {
+        "logo": "/eye_logo.png",
+        "imageAltText": "logo",
+        "websiteName": "SI HER",
+        "links": [
+            "VALUE",
+            "MY MEDIA",
+            "VISION",
+            "CV",
+            "CONNECT"
+        ],
+        "buttonText": "LOGIN",
+        "fixedHeader": true
+    },
+    "landing": {
+        "title": "I’M KARA,",
+        "subTitle": "& I CREATE EQUITABLE PLATFORMS FOR THE NEW ECONOMY.",
+        "hashTagTitle": "WHAT I STAND FOR:",
+        "hashTags": [
+            "COLLABORATION",
+            "EQUITY",
+            "IMPACT",
+            "DECENTRALIZATION",
+            "EDUCATION"
+        ],
+        "categories": [
+            {
+                "title": "Region",
+                "text": "Latin America"
+            },
+            {
+                "title": "Web3 Category",
+                "text": "Web3 Founder"
+            },
+            {
+                "title": "Organization Affiliations",
+                "text": "Web3 Founder"
+            },
+            {
+                "title": "Community Affiliations",
+                "text": "Si Her, WeDAO Latam, EcstaSHE, CryptoFemale, Web3 Ladies"
+            }
+        ],
+        "userimg": "https://salmon-capable-bee-381.mypinata.cloud/ipfs/QmWj5ZD8uHFXCMkhWcRZsZsEMyC28wuLXuZU5BUVEVRKqj",
+        "name": "Kara Howard",
+        "pronoun": "SHE/HER",
+        "marquee": [
+            "Self-Realization",
+            "Emerging Market Opportunities",
+            "DECENTRALIZING CURRENCIES & TECHNOLOGIES",
+            "COLLABORATIVE WEB3-4 ECOSYSTEM GROWTH",
+            "Inclusive Platforms"
+        ]
+    },
+    "value": {
+        "title": "MY, VALUE",
+        "description": "My, career began as an Equity Research analyst on Wall St. I started the MBA program at NYU in the Fall of 2008, right as the market crashed, with Occupy Wall St. protests happening outside our campus doors. As often happens, times of crises create opportunity and I shifted my career focus from finance to marketing & entrepreneurship. I began my entrepreneurial journey with ups and downs and great learning - most importantly, discovering my passion and purpose in supporting women in elevating their voices and professional and personal success. I have led and participated in women-related communities in investing, startups, technology, AI, futurism, product and data science. I have placed over 350 women in speaking opportunities and experience shared value and fulfillment when I see women stepping into their full potential. I bring the value of ten years in finance, and ten years of community architecting & growth marketing, to Si Her as it`s platform creator. And my exploration in Equity Research continues.",
+        "TVName": "SI HER TV",
+        "video": "https://lvpr.tv?v=33e86xt51h1f718x",
+        "links": [
+            {
+                "title": "WE ARE SI3",
+                "link": "https://www.si-her.live/"
+            },
+            {
+                "title": "UNLOCKING NFT'S FOR META IMPACT",
+                "link": "https://www.youtube.com/watch?v=y-Bz60q298c"
+            },
+            {
+                "title": "DIVERSITY IN THE NEW ECONOMY",
+                "link": "https://podcasters.spotify.com/pod/show/b-r-i/episodes/Diversity-in-the-new-economy-e26pn30/a-aa40rlt"
+            }
+        ],
+        "buttonText": "Support Kara in cryto"
+    },
+    "vision": {
+        "title": "Rizwan's Value",
+        "description": "Rizwan is a envision a near-term future that is operating with values based on inclusion, equity, and decentralized decision-making. I am hopeful to support in creating platforms that streamline cooperation and accelerate our human potential."
+    },
+    "CV": {
+        "title": "CV Highlights",
+        "highlights": [
+            {
+                "year": "2002-10",
+                "text": "EQUITY RESEARCH ASSOCIATE / FINANCIAL ANALYST"
+            },
+            {
+                "year": "2004",
+                "text": "BSC FROM UW-MADISON - PERSONAL FINANCE"
+            },
+            {
+                "year": "2012",
+                "text": "MBA FROM NYU STERN - MARKETING & ENTREPRENEURSHIP"
+            },
+            {
+                "year": "2015-19",
+                "text": "VP OF GROWTH & PARTNERSHIPS AT CLEVERTAP"
+            },
+            {
+                "year": "2017-21",
+                "text": "MANAGED THE FEMININE INTELLIGENCE"
+            },
+            {
+                "year": "2022",
+                "text": "PERSONAL DEVELOPMENT RETREAT"
+            },
+            {
+                "year": "2023",
+                "text": "INITIATED SI3 & SI HER TALENT COLLECTIVE"
+            }
+        ]
+    },
+    "available": {
+        "title": "I’M AVAILABLE FOR",
+        "marque": [
+            {
+                "heading": "speaking",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "Advising",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "Collabs",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "speaking",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "Advising",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "Collabs",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "speaking",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "Advising",
+                "image": "https://something.com/eye_logo.png"
+            },
+            {
+                "heading": "Collabs",
+                "image": "https://something.com/eye_logo.png"
+            }
+        ],
+        "description": "You are viewing an ENS domain, which is a distributed and open naming system based on the Ethereum blockchain. This website is hosted with Pinata on the IPFS, or InterPlanetary File System, which is a peer-to-peer file sharing network. The limo domain extension is a privacy-preserving ENS gateway for resolving/accessing ENS records/domains & IPFS/internet 3.0 content. For a complete web3 experience, we recommend viewing this site with a Metamask extension or Brave browser.",
+        "linkedin": {
+            "url": "https://www.linkedin.com/in/decentralizing/",
+            "hide": false
+        },
+        "hey": {
+            "url": "https://www.linkedin.com/in/decentralizing/",
+            "hide": false
+        },
+        "email": {
+            "address": "kara@si3.space",
+            "hide": false
+        }
+    }
+})
+  return res.send(renderedTemplate)
 });
 
 router.get("/:subdomain", async (req, res) => {
