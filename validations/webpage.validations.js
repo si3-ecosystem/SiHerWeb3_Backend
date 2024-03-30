6df085637ddc7dce91bf239f0d7c3b168dc7488c
RizwanAmjad <rizwanamjadms@gmail.com>
2024-03-30 14:55:03 +0500

    Updated the webpage validations according to the new template

--- validations/webpage.validations.js
+++ validations/webpage.validations.js
@@ -1,18 +1,14 @@
-const Joi = require("joi");
-const { validate } = require(".");
+const Joi = require("joi")
+const { validate } = require(".")
 
 const createWebpageSchema = Joi.object({
   navbar: Joi.object({
-    logo: Joi.string().uri().required().label("Logo"),
-    imageAltText: Joi.string().required().label("Image alt text"),
     websiteName: Joi.string().required().label("Website Name"),
     links: Joi.array()
       .items(Joi.string().required())
       .required()
       .min(1)
       .label("Links"),
-    buttonText: Joi.string().required().label("Button Text"),
-    fixedHeader: Joi.boolean().required().label("Fixed Header"),
   }),
   landing: Joi.object({
     title: Joi.string().required().label("Title"),
@@ -27,30 +23,23 @@ const createWebpageSchema = Joi.object({
       .items(
         Joi.object({
           title: Joi.string().required().label("Category Title"),
-          text: Joi.string().required().label("Category Text"),
         }).required()
       )
       .required()
       .min(1)
       .label("Categories"),
-        userimg: Joi.string().uri().required().label("User Image"),
+    userimg: Joi.string().uri().required().label("User Image"),
     name: Joi.string().required().label("Name"),
     pronoun: Joi.string().required().label("Pronoun"),
-    marquee: Joi.array()
-      .items(Joi.string().required())
-      .required()
-      .min(1)
-      .label("Marquee"),
   }),
   value: Joi.object({
     title: Joi.string().required().label("Value Title"),
     description: Joi.string().required().label("Value Description"),
-    TVName: Joi.string().required().label("TV Name"), 
+    TVName: Joi.string().required().label("TV Name"),
     video: Joi.string().uri().required().label("Video"),
     links: Joi.array()
       .items({
         title: Joi.string().required().label("Link Title"),
-        link: Joi.string().uri().required().label("Link"),
       })
       .required()
       .min(1),
@@ -61,42 +50,60 @@ const createWebpageSchema = Joi.object({
     description: Joi.string().required().label("Vision Description"),
   }),
   CV: Joi.object({
-    title: Joi.string().required().label("CV Title"),
-    highlights: Joi.array().items(
-      Joi.object({
-        year: Joi.string().required().label("Highlights year"),
-        text: Joi.string().required().label("Highlights Text"),
-      }).required()
-    ),
+    present: Joi.object({
+      title: Joi.string().required().label("CV Title"),
+      highlights: Joi.array().items(
+        Joi.object({
+          year: Joi.string().required().label("Highlights year"),
+          text: Joi.string().required().label("Highlights Text"),
+        }).required()
+      ),
+    })
+      .required()
+      .label("Present"),
+    past: Joi.object({
+      title: Joi.string().required().label("Past Title"),
+      highlights: Joi.array().items(Joi.string().required()),
+    })
+      .required()
+      .label("Past"),
+    future: Joi.object({
+      title: Joi.string().required().label("Future Title"),
+      text: Joi.string().required().label("Future Text"),
+    })
+      .required()
+      .label("Future"),
   }),
   available: Joi.object({
     title: Joi.string().required().label("Title"),
     marque: Joi.array().items(
       Joi.object({
         heading: Joi.string().required().label("Heading"),
-        image: Joi.string().uri().required().label("Image"),
       })
     ),
-    description: Joi.string().required().label("Description"),
     linkedin: Joi.object({
-      url: Joi.string().uri().label("Url"),
+      url: Joi.string().label("Linkedin Url"),
       hide: Joi.boolean().label("Hide"),
     }),
-    hey: Joi.object({
-      url: Joi.string().uri().label("Url"),
+    instagram: Joi.object({
+      url: Joi.string().label("Instagram Url"),
+      hide: Joi.boolean().label("Hide"),
+    }),
+    twitter: Joi.object({
+      url: Joi.string().label("Twitter Url"),
       hide: Joi.boolean().label("Hide"),
     }),
     email: Joi.object({
-      address: Joi.string().email().label("Url"),
+      address: Joi.string().email().label("Email Address"),
       hide: Joi.boolean().label("Hide"),
     }),
   }),
-});
+})
 
 function validateCreateWebpage(data) {
-  return validate(data, createWebpageSchema);
+  return validate(data, createWebpageSchema)
 }
 
 module.exports = {
   validateCreateWebpage,
-};
+}
