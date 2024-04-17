6172dc1f0150e068616128fdb65629afb401944f
RizwanAmjad <rizwanamjadms@gmail.com>
2024-04-17 16:51:18 +0500

    Added the support for google translate in the generated pages

--- routes/webpage.routes.js
+++ routes/webpage.routes.js
@@ -35,9 +35,9 @@ router.post("/", auth, async (req, res) => {
   const file = new File([fileBlob], `${uuidv4()}.html`, {
     type: "text/html",
   });
-console.log(file);
+
   const cid = await uploadToFileStorage(file);
-console.log(cid);
+
   const webpage = new Webpage({
     user: user._id,
     cid,
@@ -73,24 +73,32 @@ router.put("/", auth, async (req, res) => {
   const { cid } = dbWebpage;
   await deleteFromFileStorage(cid);
 
-  const jsonData = JSON.stringify(body);
-  const fileBlob = new Blob([jsonData], { type: "application/json" });
-  const file = new File([fileBlob], `${uuidv4()}.json`, {
-    type: "application/json",
+  const templateFile = fs.readFileSync(`${__dirname}/../template/index.ejs`)
+
+  const template = ejs.compile(templateFile.toString())
+  const renderedTemplate = template(body) 
+
+  const fileBlob = new Blob([renderedTemplate], { type: "text/html" });
+  const file = new File([fileBlob], `${uuidv4()}.html`, {
+    type: "text/html",
   });
 
   const newCid = await uploadToFileStorage(file);
 
-  const webpage = await Webpage.findByIdAndUpdate(dbWebpage._id, {
-    cid: newCid,
-  });
-  await webpage.save();
+  const webpage = await Webpage.findByIdAndUpdate(
+    dbWebpage._id,
+    {
+      cid: newCid,
+    },
+    { new: true }
+  )
+  await webpage.save()
 
   if(webpage.subdomain){
     await registerSubdomain(webpage.subdomain)
   }
 
-  return res.send({ webpage });
+  return res.send({ url: `${FLEEK_GATEWAY}/${webpage?.cid}`, ...webpage?.toJSON() });
 });
 
 router.delete("/", auth, async (req, res) => {
